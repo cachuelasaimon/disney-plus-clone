bae86f1b2e4508d73ce188d805ce5f2d2daed50c
cachuelasaimon <45102548+cachuelasaimon@users.noreply.github.com>
2024-07-16 17:34:05 +0800

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,3 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCtBZM-es9nOnCVWRCab0ftWQuoJGuL7yo",
-  authDomain: "disney-plus-clone-fc9bc.firebaseapp.com",
-  projectId: "disney-plus-clone-fc9bc",
-  storageBucket: "disney-plus-clone-fc9bc.appspot.com",
-  messagingSenderId: "390886491402",
-  appId: "1:390886491402:web:2afe3394127357ae14e6f3",
-  measurementId: "G-30WFJJHDGP",
-};
+const creds = process.env.REACT_APP_FIREBASE_CREDS
+
+export const firebaseConfig = JSON.parse(creds)
